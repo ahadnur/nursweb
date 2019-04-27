9c6035ea77852d92e38b01650a1248763b6ada11
endox <ahadnur44@gmail.com>
2019-04-27 19:32:01 +0600

    updated

--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from posts.models import Post
+from programming.models import Program
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def index(request):
     posts = Post.objects.order_by('-post_time').filter(is_published=True)
-
-    paginator = Paginator(posts, 3)
+    
+    paginator = Paginator(posts, per_page=3)
     page = request.GET.get('page')
     paged_posts = paginator.get_page(page)
 
     context = {
-        'posts': paged_posts 
+        'posts': paged_posts,
     }
     return render(request, 'posts/posts.html', context)
 
