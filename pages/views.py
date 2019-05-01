9ca4af179aa1fab544faa5cc30ffdfcc4add4e5b
endox <ahadnur44@gmail.com>
2019-05-02 03:55:44 +0600

    index page pagination updated

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from posts.models import Post
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 
 def index(request):
-    posts = Post.objects.order_by('-post_time').filter(is_published=True)[:3]
+    posts = Post.objects.order_by('-post_time').filter(is_published=True)
+
+    paginator = Paginator(posts, per_page=3)
+    page = request.GET.get('page')
+    paged_posts = paginator.get_page(page)
+
     context = {
-        'posts': posts
+        'posts': paged_posts
     }
     return render(request, 'pages/index.html', context)
 
